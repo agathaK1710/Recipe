546a2c3ead82c6111ac41f10393c6524c079b393
Agatha <agatha.kazak@gmail.com>
2022-07-20 13:04:44 +0300

    change db version(add likes)

--- app/src/main/java/com/android/recipe/presentation/MainActivity.kt
+++ app/src/main/java/com/android/recipe/presentation/MainActivity.kt
@@ -7,15 +7,16 @@ import androidx.lifecycle.ViewModelProvider
 import com.android.recipe.R
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel by lazy {
-        ViewModelProvider(this)[RecipeViewModel::class.java]
-    }
+//    private val viewModel by lazy {
+//        ViewModelProvider(this)[RecipeViewModel::class.java]
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel.getRecipeInfo(632625).observe(this){
-            Log.d("MainActivity", it.toString())
-        }
+//        viewModel.getRecipeInfo(632625).observe(this){
+//            Log.d("MainActivity", it.toString())
+//        }
+        val viewModel = ViewModelProvider(this)[RecipeViewModel::class.java]
     }
 }
